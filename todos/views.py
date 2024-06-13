053eb35b128cc40ec2e1abaf1609ce6ed52e904a
Hunter Wallen <hunterwallen67@gmail.com>
2024-06-12 18:03:36 -0600

    deployment

---
+++ todos/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Todo
+from rest_framework import viewsets, permissions
+from .serializers import TodoSerializer
+
+
+class TodoViewset(viewsets.ModelViewSet):
+  queryset = Todo.objects.all()
+  serializer_class = TodoSerializer
+  permission_classes = [permissions.AllowAny]
